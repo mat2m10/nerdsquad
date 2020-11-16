d359acabb2a38a1d57a2d243dd679156ea8d1c03
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2020-11-16 18:20:30 +0100

    JS debugging

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,10 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { toggleVisibility } from "../plugins/toggleVisibility"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  toggleVisibility();
 });
