abdaeb450a95d36ba176307f1f8064c1bd76c306
Nicolas Eloy <colasrsca@gmail.com>
2020-11-18 17:36:37 +0100

    dice number

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_11_18_094559) do
+ActiveRecord::Schema.define(version: 2020_11_18_162352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_094559) do
     t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "number_of_dices"
     t.index ["game_id"], name: "index_dices_on_game_id"
   end
 
