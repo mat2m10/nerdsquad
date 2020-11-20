167228a4249cec81cb713a5c425faf00cd948be3
Pierre Lion <pierrelion1997@yahoo.fr>
2020-11-20 15:36:25 +0100

    changes to show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :boards, only: [:new, :create, :show, :update, :destroy]
     resources :dices, only: [:new, :create, :show, :update]
     resources :card_decks, only: [:new, :create, :show, :update] do
-      resources :cards, only: [:new, :create, :show, :update]
+      resources :cards, only: [:new, :create, :show, :update, :destroy]
     end
     resources :pieces, only: [:new, :create, :show, :edit, :update, :destroy]
     resources :tokens, only: [:new, :create, :show, :edit, :update, :destroy]
