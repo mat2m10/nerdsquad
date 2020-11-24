32cc2222922536ce523999e36ab298baa575c78b
Raphael Marsan <marsanraphael@gmail.com>
2020-11-24 11:56:00 +0100

    hi

--- app/javascript/plugins/resizing.js
+++ app/javascript/plugins/resizing.js
@@ -1,11 +1,14 @@
 
-const incsize = (e) => {
-    e.preventDefault()
+const incsize = () => {
     let el = document.querySelectorAll(".piece");
     el.forEach((i) => {
-        let theheight = i.height;
-        let newHeight = theheight/100 + 1;
-        i.style.transform = `scale(${newHeight})`;
+        let height = i.getAttribute("height")
+        console.log(height)
+        let heightNumber = height.substr(0, height.indexOf('p'))
+        let integer = parseInt(heightNumber, 10)
+        console.log(integer)
+        console.log(i.getAttribute("height"))
+        i.setAttribute("height", i.getAttribute("height") + 200 + 'px' );
     })
 }
 const decsize = (e) => {
