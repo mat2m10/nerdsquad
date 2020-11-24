0c71e69275b405b8fd23cf430fc2e7cc43ba9790
Aurelie Rousseau <rousseau.au@gmail.com>
2020-11-24 16:16:26 +0100

    s

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,24 +1,22 @@
 function navbar() {
-      const element = document.querySelector('.menu-icon');
-      element.addEventListener('click', () => {        
-        element.classList.toggle("showing");        
+  const element = document.querySelector('.menu-icon');
+  element.addEventListener('click', () => {        
+  element.classList.toggle("showing");        
+});
+
+  document.addEventListener('scroll', (event) => {
+    console.log(window.pageYOffset)
+    const menu = document.querySelector('.menu');
+          
+      if (window.pageYOffset >= 190){
+        console.log("hello")
+        menu.classList.add('black');
+      }
+      else {              
+        menu.classList.remove('black');
+      }      
+          
       });
-
-      document.addEventListener('scroll', (event) => {
-            console.log(window.pageYOffset)
-            const menu = document.querySelector('.menu');
-          
-            if (window.pageYOffset >= 276){
-             console.log("hello")
-              menu.classList.add('black');
-              }
-            else {
-              
-              menu.classList.remove('black');
-              }      
-          
-          });
 };
-
 
 export { navbar };
