cac84efcd2f4dd14ff0db7489fc654225d19b936
Aurelie Rousseau <rousseau.au@gmail.com>
2020-11-24 16:59:42 +0100

    false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.serve_static_files = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
