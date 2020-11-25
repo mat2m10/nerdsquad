93f52a5230724233e071969bc2df79703de53cde
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2020-11-25 17:54:46 +0100

    working dices

--- app/controllers/dices_controller.rb
+++ app/controllers/dices_controller.rb
@@ -2,25 +2,31 @@ class DicesController < ApplicationController
 
   def new
     @game = Game.find(params[:game_id])
-    @dice = Dice.create(game: @game)
+    @dice = Dice.new
   end
 
   def create
-    @dice = Dice.find(params[:id])
-    number_of_dices = @dice.number_of_dices.to_i
+    @game = Game.find(params[:game_id])
+    number_of_dices = params[:dice][:number_of_dices].to_i
     number_of_dices.times do
-      dice = Dice.new(dice_params)
-      dice.game = @game
-      dice.save
+      @dice = Dice.new(dice_params)
+      @dice.game = @game
+      @dice.save
     end
-    redirect_to game_dice_path
+    redirect_to game_dice_path(@game, @dice)
   end
 
   def update
     @game = Game.find(params[:game_id])
     @dice = @game.dices.last
     @dice.update(dice_params)
-    redirect_to game_dice_path(@game, @dice)
+    redirect_to request.referrer and return unless request.referrer.include? 'dice'
+
+    num = @dice.number_of_dices
+    @game.dices[-num..].each do |dice|
+      dice.assign_attributes(posX: @dice.posX + num * 100, posY: @dice.posY + num * 100)
+    end
+    redirect_to request.referrer
   end
 
   def show
@@ -30,6 +36,6 @@ class DicesController < ApplicationController
   private
 
   def dice_params
-    params.require(:dice).permit(:faces, :posX, :posY, :photo, :number_of_dices)
+    params.require(:dice).permit(:faces, :posX, :posY, :photo, :number_of_dices, :value)
   end
 end
