a161845f80adfad16c353307ee1bac73f7a2ff5b
Raphael Marsan <marsanraphael@gmail.com>
2020-12-15 20:47:45 +0100

    dependent destroy between user & games

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :games
+  has_many :games, dependent: :destroy
   has_many :connections
   has_many :gamerooms, through: :connections
   has_one_attached :photo
